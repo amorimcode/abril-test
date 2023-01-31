0ace5cc03fc8d26efc56e257983962521dd3845d
amorimcode <bruno.amorim032@gmail.com.br>
2023-01-31 14:21:15 -0300

    chore: project config

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  const test = "teste";
+  console.log(test);
+
+  return;
+  <View>
+    <Text>Home</Text>
+  </View>;
+};
+
+export default Home;
