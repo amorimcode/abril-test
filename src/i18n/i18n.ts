759e6e3f1caceedd8f6ed23761b00385a5ff91e8
amorimcode <bruno.amorim032@gmail.com>
2023-02-03 22:50:08 -0300

    feat: finish translate config getting device locale

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,9 +4,11 @@ import { initReactI18next } from 'react-i18next';
 import ptbr from './ptbr.json';
 import en from './en.json';
 
+import * as Localization from 'expo-localization';
+
 i18n.use(initReactI18next).init({
   compatibilityJSON: 'v3',
-  lng: 'ptbr',
+  lng: Localization.locale,
   fallbackLng: 'ptbr',
   resources: {
     ptbr: {
